4c86f5de4162ebcb7a3888879488c518b16ef8a8
d.luque <d.luque@edu.uah.es>
2024-11-19 19:00:33 +0100

    commit

--- PECL2.sql
+++ PECL2.sql
@@ -1,1 +1,31 @@
 
+create database PECL2;
+
+create schema temporal;
+
+create schema final;
+
+CREATE TABLE IF NOT EXISTS Vehiculos(
+    vehicle_id  TEXT,
+    state_registration  TEXT,
+    vehicle_type    TEXT,
+    vehicle_make    TEXT,
+    vehicle_model   TEXT,
+    vehicle_year    TEXT
+);
+
+CREATE TABLE IF NOT EXISTS personas(
+
+);
+
+CREATE TABLE IF NOT EXISTS colision_persona(
+
+);
+
+create table colision_vehiculo(
+
+);
+
+create table accidente(
+
+);
