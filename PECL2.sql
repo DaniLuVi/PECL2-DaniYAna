66854cc76c34939cbb36766a23064c0072e0f806
d.luque <d.luque@edu.uah.es>
2024-11-26 18:05:58 +0100

    ejecutadas las tablas, falta hacer el copy para insertar los archivos .csv a las tablas

--- PECL2.sql
+++ PECL2.sql
@@ -1,6 +1,7 @@
 
 create database PECL2;
 
+
 create schema temporal;
 
 create schema final;
@@ -160,7 +161,7 @@ CREATE TABLE IF NOT EXISTS final.colision_persona (
    person_id UUID,
    person_type VARCHAR(15),
    person_injury VARCHAR(15),
-   vehicle_id INT UNIQUE,
+   vehicle_id VARCHAR(50) UNIQUE,
    person_age INT,
    ejection VARCHAR(15),
    emotional_status VARCHAR(25),
@@ -174,11 +175,11 @@ CREATE TABLE IF NOT EXISTS final.colision_persona (
    contributing_factor_1 VARCHAR(25),
    contributing_factor_2 VARCHAR(25),
    person_sex CHAR(1),
-    CONSTRAINT Vehiculo_pk FOREIGN KEY (vehicle_id) REFERENCES vehiculo (vehicle_id) MATCH FULL
+    CONSTRAINT Vehiculo_pk FOREIGN KEY (vehicle_id) REFERENCES final.vehiculo (vehicle_id) MATCH FULL
        ON DELETE SET DEFAULT ON UPDATE SET DEFAULT,
-    CONSTRAINT Person_pk FOREIGN KEY (person_id) REFERENCES persona (person_id) MATCH FULL
+    CONSTRAINT Person_pk FOREIGN KEY (person_id) REFERENCES final.persona (person_id) MATCH FULL
        ON DELETE SET DEFAULT ON UPDATE SET DEFAULT,
-    CONSTRAINT Unique_pk PRIMARY KEY (unique_id)    -- seria unique_id o collision_id
+    CONSTRAINT Unique_pk1 PRIMARY KEY (unique_id)    -- seria unique_id o collision_id
 );
 
 
@@ -187,7 +188,7 @@ CREATE TABLE IF NOT EXISTS final.colision_vehiculo (
    collision_id INT UNIQUE,
    crash_date DATE,
    crash_time TIME without time zone,
-   vehicle_id UUID UNIQUE,
+   vehicle_id VARCHAR(50) UNIQUE,
    state_registration VARCHAR(2),
    vehicle_type VARCHAR(50),
    vehicle_make VARCHAR(15),
@@ -208,11 +209,11 @@ CREATE TABLE IF NOT EXISTS final.colision_vehiculo (
    public_property_damage_type VARCHAR(50),
    contributing_factor_1 VARCHAR(100),
    contributing_factor_2 VARCHAR(100),
-    CONSTRAINT Vehiculo_pk FOREIGN KEY (vehicle_id) REFERENCES vehiculo (vehicle_id) MATCH FULL
+    CONSTRAINT Vehiculo_pk FOREIGN KEY (vehicle_id) REFERENCES final.vehiculo (vehicle_id) MATCH FULL
        ON DELETE SET DEFAULT ON UPDATE SET DEFAULT,
-    CONSTRAINT Collision_pk FOREIGN KEY (collision_id) REFERENCES accidentes (collision_id) MATCH FULL
+    CONSTRAINT Collision_pk FOREIGN KEY (collision_id) REFERENCES final.accidentes (collision_id) MATCH FULL
        ON DELETE SET DEFAULT ON UPDATE SET DEFAULT,
-    CONSTRAINT Unique_pk PRIMARY KEY (unique_id)        -- seria unique_id o collision_id
+    CONSTRAINT Unique_pk2 PRIMARY KEY (unique_id)        -- seria unique_id o collision_id
 
 );
 
@@ -242,3 +243,4 @@ CREATE TABLE IF NOT EXISTS final.vehiculo (
    vehicle_make VARCHAR(25),
    CONSTRAINT Vehiculo_pk PRIMARY KEY (vehicle_id)
 );
+
