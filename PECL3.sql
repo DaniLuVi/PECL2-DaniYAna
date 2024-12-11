31b6f279eaf4b0861a931556b36b88b77b5a2ea8
analuciaguerrero <157589298+analuciaguerrero@users.noreply.github.com>
2024-12-11 12:50:18 +0100

    Estoy empezando con los trigger

--- PECL3.sql
+++ PECL3.sql
@@ -18,9 +18,9 @@ where person_id is NULL or person_id like '' or length(person_id) < 10;
 INSERT INTO pecl2.final.vehiculo
 (select colision_vehiculo.state_registration
  from pecl2.final.colision_vehiculo, pecl2.final.vehiculo
- where colision_vehiculo.vehicle_id = vehiculo.vehicle_id)
+ where colision_vehiculo.vehicle_id = vehiculo.vehicle_id);
 
-DELETE FROM pecl2.final.colision_vehiculo.state_registration;
+ALTER TABLE pecl2.final.colision_vehiculo DROP COLUMN state_registration;
 
 -- ejercicio 4
 
@@ -45,10 +45,23 @@ SET state_registration = 'unknown'
 WHERE state_registration IS NULL OR state_registration LIKE '';
 
 -- ejercicio 5
+INSERT INTO pecl2.final.persona
+(select pecl2.final.colision_persona.person_sex
+ from pecl2.final.colision_persona, pecl2.final.persona
+ where pecl2.final.colision_persona.person_id = pecl2.final.persona.person_id);
 
+ALTER TABLE pecl2.final.colision_persona DROP COLUMN person_sex;
+
+UPDATE pecl2.final.persona
+SET person_sex = 'U'
+WHERE person_sex is NULL or person_sex LIKE '';
 
 -- ejercicio 6
-
+ALTER TABLE pecl2.final.persona ADD COLUMN person_age;
+CREATE TRIGGER calcular_edad
+    BEFORE INSERT ON persona
+    FOR EACH ROW
+    BEGIN
 
 -- ejercicio 7
 
