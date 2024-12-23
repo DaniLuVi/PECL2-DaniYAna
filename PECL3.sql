38dc1145bc6d01db4bd781766a1bebfaf8d21739
d.luque <d.luque@edu.uah.es>
2024-12-24 00:53:06 +0100

    arreglados los ejercicicios 3 y 5. Falta arreglar uno del ejercicio 4 que todavia da problemas, y revisar que lo de los ejercicios de los triggers esten bien realizados

--- PECL3.sql
+++ PECL3.sql
@@ -14,13 +14,18 @@ ALTER TABLE pecl3.final.colision_persona DROP COLUMN vehicle_id;
 -- ejercicio 2      HECHO BIEN
 DELETE FROM pecl3.final.persona WHERE pecl3.final.persona.person_id IS NULL OR pecl3.final.persona.person_id LIKE '' OR length(pecl3.final.persona.person_id) < 10;
 
--- ejercicio 3      NO LO HACE BIEN
-INSERT INTO pecl3.final.vehiculo
-(select colision_vehiculo.state_registration
- from pecl3.final.colision_vehiculo, pecl3.final.vehiculo
- where pecl3.final.colision_vehiculo.vehicle_id = pecl3.final.vehiculo.vehicle_id);
+-- ejercicio 3      HECHO BIEN
+UPDATE pecl3.final.vehiculo
+SET state_registration = pecl3.final.colision_vehiculo.state_registration
+FROM pecl3.final.colision_vehiculo
+WHERE pecl3.final.vehiculo.vehicle_id = pecl3.final.colision_vehiculo.vehicle_id;
 
 ALTER TABLE pecl3.final.colision_vehiculo DROP COLUMN state_registration;
+
+--INSERT INTO pecl3.final.vehiculo
+--(select colision_vehiculo.state_registration
+ --from pecl3.final.colision_vehiculo, pecl3.final.vehiculo
+ --where pecl3.final.colision_vehiculo.vehicle_id = pecl3.final.vehiculo.vehicle_id);
 
 -- ejercicio 4      FUNCIONAN BIEN TODOS MENOS EL DE VEHICLE_YEAR
 
@@ -38,23 +43,29 @@ WHERE vehicle_model IS NULL OR vehicle_model LIKE '';
 
 UPDATE pecl3.final.vehiculo
 SET vehicle_year = 9999
-WHERE vehicle_year IS NULL OR vehicle_year LIKE '' OR vehicle_year == 'null';   -- ARREGLAR
+WHERE vehicle_year IS NULL OR vehicle_year LIKE '' OR vehicle_year = 'null';   -- ARREGLAR
 
 UPDATE pecl3.final.vehiculo
 SET state_registration = 'unknown'
-WHERE state_registration IS NULL OR state_registration LIKE '';
+WHERE state_registration IS NULL OR state_registration = '';
 
--- ejercicio 5
-INSERT INTO pecl3.final.persona
-(select pecl3.final.colision_persona.person_sex
- from pecl3.final.colision_persona, pecl3.final.persona
- where pecl3.final.colision_persona.person_id = pecl3.final.persona.person_id);
+-- ejercicio 5      HECHO BIEN
+
+UPDATE pecl3.final.persona
+SET person_sex = pecl3.final.colision_persona.person_sex
+FROM pecl3.final.colision_persona
+WHERE pecl3.final.persona.person_id = pecl3.final.colision_persona.person_id;
 
 ALTER TABLE pecl3.final.colision_persona DROP COLUMN person_sex;
 
 UPDATE pecl3.final.persona
 SET person_sex = 'U'
 WHERE person_sex is NULL or person_sex LIKE '';
+
+--INSERT INTO pecl3.final.persona
+--(select pecl3.final.colision_persona.person_sex
+ --from pecl3.final.colision_persona, pecl3.final.persona
+ --where pecl3.final.colision_persona.person_id = pecl3.final.persona.person_id);
 
 -- ejercicio 6
 ALTER TABLE pecl3.final.persona ADD COLUMN person_age INT;
